deefc9aebd931d8435accf4402640147a138a917
vitordev <vitor.mancio@gmail.com>
2023-01-30 23:45:57 -0300

    [refactor/style] refactor style

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,38 @@ import {
   Card,
   Text,
   Textarea,
-  Image
+  Image,
+  createTheme
 } from "@nextui-org/react"; 
+import {
+  moreStyleCards,
+  moreStyleContainer,
+  WrapHeaderCard,
+} from "./styles/App";
 import { useEffect, useState } from "react";
 import parse from 'html-react-parser';
+import w3dLogo from './assets/w3d-logo.png'
 
 export function App() {
     const [tokenUri, setTokenUri] = useState("");
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
+
+    function renderData() {
+      return (
+        <>
+          <p>
+            <strong>Nome</strong> ⬇️
+            <br /> {name}
+          </p>
+          <p>
+            <strong>Description</strong> ⬇️
+            <br /> {description}
+          </p>
+        </>
+      );
+    }
 
     useEffect(() => {
       previewNft();
@@ -41,33 +63,44 @@ export function App() {
         );
         let img = atob(imgBase64);
         setImage(img);
+        setParsedImage(parse(image));
       } catch (e) {
         console.error(e);
       }
     }
 
+    const darkTheme = createTheme({
+      type: 'dark'
+    });
+
     return (
-    <NextUIProvider>
-      <Container>
-        <Card>
-          <Text h1>NFT Preview</Text>
-          <Text>
-           Paste your tokenURI bellow:
-          </Text>
-
-          <Textarea
-            label="TokenUri Code:"
-            placeholder="Paste your code here !"
-            onChange={handleMessageChange}
-          />
-
-          <Text> Nome: {name} </Text>
-          <Text> Description: {description} </Text>
-          { <div style={{width:"320px", height:"400px"}}>{parse(image)}</div> }
-        </Card>
-      </Container>
-    </NextUIProvider>
-  );
+      <NextUIProvider theme={darkTheme}>
+        <Container css={moreStyleContainer}>
+          <Card css={moreStyleCards}>
+            <WrapHeaderCard>
+              <Image src={w3dLogo} width={35} height={35} />
+              <Text h1 size={"$4xl"}>
+                NFT Preview
+              </Text>
+            </WrapHeaderCard>
+            <Textarea
+              label="Paste your TokenUri code here👇"
+              onChange={handleMessageChange}
+              status="secondary"
+              bordered
+              isHoverable
+              css={{ marginBottom:'$10' }}
+            />
+            {name.length > 2 && renderData()}
+            {
+              <div style={{ width: "300px", height: "300px", margin:'auto', marginTop:'2rem' }}>
+                {parse(image)}
+              </div>
+            }
+          </Card>
+        </Container>
+      </NextUIProvider>
+    );
 }
 
 
