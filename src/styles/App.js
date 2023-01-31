3019f22e735a2b418d647926131a28e20b15b048
vitordev <vitor.mancio@gmail.com>
2023-01-30 23:44:09 -0300

    create style js file

---
+++ src/styles/App.js
@@ -0,0 +1,23 @@
+import { styled } from "@nextui-org/react";
+
+export const moreStyleContainer = {
+
+}
+
+export const moreStyleCards = {
+  textAlign: "center",
+  padding:'27px',
+  height:'auto',
+  background:'transparent'  
+}
+
+export const WrapHeaderCard = styled('div',{
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'column'
+})
+
+export const ImageContainer = styled('div', {
+    margin:'auto'
+})
